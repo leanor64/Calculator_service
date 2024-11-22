3bffb7050a80d20df232517b454cfb14e6be0d15
leanor64 <113692306+leanor64@users.noreply.github.com>
2024-11-22 08:29:04 +0100

    Update CalculatorService.js

--- CalculatorService.js
+++ CalculatorService.js
@@ -4,10 +4,10 @@ var request = require('sync-request');
 const PORT = 50050;
 const service_ip = '192.168.37.16';
 
-const SUM_SERVICE_IP_PORT = 'http://'+service_ip+':50001';
-const SUB_SERVICE_IP_PORT = 'http://'+service_ip+':50002';
-const MUL_SERVICE_IP_PORT = 'http://'+service_ip+':50003';
-const DIV_SERVICE_IP_PORT = 'http://'+service_ip+':50004';
+const SUM_SERVICE_IP_PORT = 'http://'+service_ip+':31001';
+const SUB_SERVICE_IP_PORT = 'http://'+service_ip+':31002';
+const MUL_SERVICE_IP_PORT = 'http://'+service_ip+':31003';
+const DIV_SERVICE_IP_PORT = 'http://'+service_ip+':31004';
 
 
 
